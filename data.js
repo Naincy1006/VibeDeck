b985ce4ed0292b99b43c08cfbbacb93ae3b85c6c
nancyy <ynaincy4510@gmail.com>
2026-07-04 11:24:45 +0530

    Modify UI Changes and add Filtering logic

--- data.js
+++ data.js
@@ -1,16 +1,16 @@
 const MEDIA_DATA=[
     {
-        id: "inception",
+        id: 1,
         title:"Inception",
         type: "movie",
         img: "assets/inception.jpg",
-        vibes: ["Thought-Provoking", "Sci-fi","Dreams"],
+        vibes: ["Thought-Provoking", "Sci-fi","Dreams","Action"],
         description: "A thief who steals corporate secrets through dream-sharing technology is given by an inverse task of planting an idea.",
         wiki: "https://en.wikipedia.org/wiki/Inception",
         learnMore : "https://youtu.be/YoHD9XEInc0?si=EqVeNdGiRTHk2KF2"
     },
     {
-        id: "love_rosie",
+        id: 2,
         title: "Love, Rosie",
         type: "movie",
         img: "assets/love_rosie.jpg",
@@ -20,17 +20,17 @@ const MEDIA_DATA=[
         learnMore: "https://youtu.be/5zL3YJKygd4?si=b1GDCq1fCM71a3mU"
     },
     {
-        id: "all_of_us_are_dead",
+        id: 3,
         title: "All of Us Are Dead",
         type: "series",
         img: "assets/dead.jpg",
-        vibes: ["Survival Chaos","Zombie Outbreak","School Apocalypse","Friendship Under Pressure"],
+        vibes: ["Survival","Zombie Outbreak","Apocalypse","Friendship"],
         description: "Students trapped in a high school fight for survival during a zombie outbreak",
         wiki:"https://en.wikipedia.org/wiki/All_of_Us_Are_Dead#" ,
         learnMore: "https://youtu.be/IN5TD4VRcSM?si=x6Q9JtATZLD-qD7n"
     },
     {
-        id: "euphoria" ,
+        id: 4 ,
         title: "Euphoria",
         type: "series",
         img: "assets/euphoria.jpg",
@@ -40,7 +40,7 @@ const MEDIA_DATA=[
         learnMore:"https://youtu.be/UR4Pxgnm-GA?si=YVkqAG_A1pnYjqfa" 
     },
     {
-        id: "toy_story" ,
+        id: 5 ,
         title:"Toy Story",
         type: "movie",
         img: "assets/toystory.jpg",
@@ -50,37 +50,37 @@ const MEDIA_DATA=[
         learnMore: "https://youtu.be/v-PjgYDrg70?si=89O44sLpp9pd_sEk"
     },
     {
-        id: "friends" ,
+        id: 6 ,
         title: "Friends",
         type: "show",
         img: "assets/friends.jpg",
-        vibes:["Comfort Sitcom", "Humour","Friendship"] ,
+        vibes:["Sitcom", "Humour","Friendship","Comfort Show"] ,
         description:"A group of six  friends navigate love, careers, and life in New York City." ,
         wiki: "https://en.wikipedia.org/wiki/Friends",
         learnMore:"https://www.imdb.com/title/tt0108778/?ref_=ext_shr_lnk" 
     },
     {
-        id: "purusit_of_happyness" ,
+        id: 7 ,
         title:"The Pursuit of Happyness" ,
         type: "movie",
         img: "assets/pursuit.jpg",
-        vibes: ["Resilience","Struggle","Poverty to Success"],
+        vibes: ["Resilience","Struggle","Poverty", "Success"],
         description:"A struggling father fights through homelessness to build a better life for his son." ,
         wiki:"https://en.wikipedia.org/wiki/The_Pursuit_of_Happyness" ,
         learnMore: "https://youtu.be/DMOBlEcRuw8?si=QLHC2nNWCi3qb1-P"
     },
     {
-        id: "atomic_habits" ,
+        id: 8,
         title:"Atomic Habits" ,
         type: "book",
         img: "assets/atomic.jpg",
-        vibes: ["Self-Help","Motivational","Incremental Growth"],
+        vibes: ["Self-Help","Motivational","Growth"],
         description: "A guide to building powerful habits through small, consistent improvements.",
         wiki:"https://en.wikipedia.org/wiki/Atomic_Habits" ,
         learnMore: " https://www.goodreads.com/book/show/40121378-atomic-habits"
     },
     {
-        id: "norwegian_wood" ,
+        id: 9,
         title:"Norwegian Wood" ,
         type: "book",
         img: "assets/woods.jpg",
@@ -90,7 +90,7 @@ const MEDIA_DATA=[
         learnMore: "https://www.goodreads.com/book/show/11297.Norwegian_Wood"
     },
     {
-        id: "kite_runner" ,
+        id: 10 ,
         title:"The Kite Runner" ,
         type: "book",
         img: "assets/runner.jpg",
@@ -100,47 +100,47 @@ const MEDIA_DATA=[
         learnMore: "https://www.goodreads.com/book/show/17165596-the-kite-runner"
     },
     {
-        id: "psychology_of_money" ,
+        id: 11 ,
         title: "The Psychology of Money",
         type: "book",
         img: "assets/money.jpg",
-        vibes: ["Motivational","Behavioural Finance","Mindset"],
+        vibes: ["Motivational","Finance","Mindset"],
         description:"Explores how human behaviour shapes financial success more than math does." ,
         wiki:"https://en.wikipedia.org/wiki/The_Psychology_of_Money" ,
         learnMore: "https://www.goodreads.com/book/show/41881472-the-psychology-of-money"
     },
     {
-        id: "boys" ,
+        id: 12 ,
         title:"The Boys" ,
         type:"series" ,
         img:"assets/boys.jpg" ,
-        vibes:["Dark Satire","Anti-Heroes","Violence","Corrupted Power"] ,
+        vibes:["Dark Satire","Anti-Heroes","Violence","Power"] ,
         description: "A dark take on superheroes where corporate power and corruption replace heroism.",
         wiki: "https://en.wikipedia.org/wiki/The_Boys_(TV_series)",
         learnMore: "https://en.wikipedia.org/wiki/The_Boys_(TV_series)"
     },
     {
-        id: "potter_philosophers's _stone" ,
+        id: 13,
         title:"Harry Potter and The Philosopher's Stone",
         type: "book",
         img: "assets/potter.jpg",
-        vibes: ["Magical Discovery","Friendship","Hidden-World","Wonder"],
+        vibes: ["Magic","Discovery","Friendship","Hidden-World","Wonder"],
         description: "A boy discovers he's a wizard and enters a magical world.",
         wiki:"https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone" ,
-        learnMore:"" 
+        learnMore:"https://www.goodreads.com/book/show/42844155-harry-potter-and-the-philosopher-s-stone" 
     },
     {
-        id: "500dos" ,
-        title: "500 Days of Summer",
+        id: 14,
+        title: "(500) Days of Summer",
         type: "movie",
         img: "assets/dos.jpg",
-        vibes: ["Emotional Disillusionment","Non-Linear Romance","Expectations vs Reality"],
+        vibes: ["Hopeless Romantic","Romance","Friendship"],
         description: "A nonlinear look at a relationship that isn't meant to be." ,
         wiki: "https://en.wikipedia.org/wiki/500_Days_of_Summer",
         learnMore: "https://youtu.be/PsD0NpFSADM?si=7-_Ben9pu63NJ4jH"
     },
     {
-        id: "shawshank_redemption" ,
+        id: 15 ,
         title: "The Shawshank Redemption",
         type: "movie",
         img: "assets/shawshank.jpg",
@@ -148,5 +148,195 @@ const MEDIA_DATA=[
         description: "A man maintains hope and dignity while serving a life sentence in prison.",
         wiki: "https://en.wikipedia.org/wiki/The_Shawshank_Redemption",
         learnMore: "https://youtu.be/NmzuHjWmXOc?si=kY8GAqxO4om8qsoS"
+    },
+    {
+        id: 16,
+        title: "Spider Man: Brand New Day",
+        type: "movie",
+        img: "assets/spidey_newday.jpg",
+        vibes: ["Superhero","Adventure"],
+        description: "Peter Parker's life is reset after major events, focusing on a fresh status quo and new challenges in his superhero identity.",
+        wiki: "https://en.wikipedia.org/wiki/Spider-Man:_Brand_New_Day",
+        learnMore: "https://youtu.be/62bIsvRcPv0?si=XQhNPtCwZIzrLwuN"
+    },
+    {
+        id: 17,
+        title: "The Odyssey",
+        type: "movie",
+        img: "assets/odyssey.jpg",
+        vibes: ["Mythology","Adventure","Fantasy"],
+        description: "Homer's epic poem about Odysseus's long, perilous journey home after the Trojan War, filled with mythic trials and gods.",
+        wiki: "https://simple.wikipedia.org/wiki/The_Odyssey_(2026_movie)",
+        learnMore: "https://youtu.be/f_bKjZeJBBI?si=IJY3QSoZztNj8N5R"
+    },
+    {
+        id: 18,
+        title: "Your Fault: London",
+        type: "movie",
+        img: "assets/yourfault.jpg",
+        vibes: ["Romance","Drama","Forbidden Love","Coming-Of-Age"],
+        description: "The forbidden love between two step siblings faces challanges as they pursue different paths, as they deal with new relationships and secrets.",
+        wiki: "https://en.wikipedia.org/wiki/Your_Fault:_London",
+        learnMore: "https://youtu.be/A3ZbjkDxYUs?si=uai1qt20Cga_YAaT"
+    },
+    {
+        id:19,
+        title: "Off Campus",
+        type: "show",
+        img: "assets/offCampus.png",
+        vibes: ["Romance","Drama","Sports","College","Fake Dating"],
+        description: "A romance series following college athletes navigating relationships, personl growth, and emotional conflict.",
+        wiki: "https://en.wikipedia.org/wiki/Off_Campus",
+        learnMore: "https://youtu.be/9mi3YVYm3ZU?si=hNXeaLwB9VHGXNtr"
+    },
+    {
+        id: 20,
+        title: "Voicemails for Isabelle",
+        type: "movie",
+        img: "assets/voicemails_isabelle.jpg",
+        vibes: ["Romance","Comedy","Drama","Grief","Family"],
+        description: "Jill leaves voicemails for her deceased sister about her chaotic life in San Francisco.Unwittingly, an Austin real estate agent begins to receive them.",
+        wiki: "https://en.wikipedia.org/wiki/Voicemails_for_Isabelle",
+        learnMore: "https://youtu.be/9mi3YVYm3ZU?si=hNXeaLwB9VHGXNtr"
+    },
+    {
+        id: 21,
+        title: "Yesteryear",
+        type: "book",
+        img: "assets/yesteryear.jpg",
+        vibes:["Satire","Fiction","Psychological","Suspense"],
+        description: "A story about Natalie Heller Mills, a 'tradwife' social media influencer, who wakes up one day in 19th century, where she must learn how to live without modern society.",
+        wiki: "https://en.wikipedia.org/wiki/Yesteryear_(novel)",
+        learnMore: "https://www.goodreads.com/book/show/238226942-yesteryear"
+    },
+    {
+        id: 22,
+        title: "The Silent Patient",
+        type: "book",
+        img: "assets/silentPatient.jpg",
+        vibes: ["Psychological Thriller","Mystery","Suspense","Fiction"],
+        description: "A woman stops speaking after a violent crime and the therapist obsessed with uncovering her truth.",
+        wiki: "https://en.wikipedia.org/wiki/The_Silent_Patient",
+        learnMore: "https://www.goodreads.com/book/show/40097951-the-silent-patient"
+    },
+    {
+        id: 23,
+        title: "Days at the Morisaki Bookshop",
+        type: "book",
+        img: "assets/Days_at_the_Morisaki_Bookshop.jpg",
+        vibes: ["Healing","Coffeehouses","Heartbreak","Slow-paced"],
+        description: "A quiet literary novel about healing and self-discovery through books and life in a secondhand bookstore.",
+        wiki: "",
+        learnMore: "https://www.goodreads.com/book/show/62047992-days-at-the-morisaki-bookshop"
+    },
+    {
+        id: 24,
+        title: "Strangers: A Memoir of Marriage",
+        type: "book",
+        img: "assets/strangers.jpg",
+        vibes: ["Betrayal","Privilege","Pandemic"],
+        description: "The author details her separation and divorce during the COVID-19 pandemic.",
+        wiki: "https://en.wikipedia.org/wiki/Strangers:_A_Memoir_of_Marriage",
+        learnMore: "https://www.goodreads.com/book/show/231108670-strangers"
+    },
+    {
+        id: 25,
+        title: "The Godfather",
+        type: "movie",
+        img: "assets/godfather.jpg",
+        vibes: ["Crime","Drama","Gangster","Thriller"],
+        description: "Crime saga about the Corleone mafia family, power, loyalty, and corruption across generations.",
+        wiki: "https://en.wikipedia.org/wiki/The_Godfather",
+        learnMore: "https://youtu.be/UaVTIH8mujA?si=_NB5UzYnzg_QqY8H"
+    },
+    {
+        id: 26,
+        title: "Parasite",
+        type: "movie",
+        img: "assets/parasite.jpg",
+        vibes: ["Psychological Thriller","Satire","Black Comedy","Horror"],
+        description: "A dark social satire about class inequality, where a poor family infiltrates a wealthy household with escxalating consequences.",
+        wiki: "assets/parasite.jpg",
+        learnMore: "https://youtu.be/5xH0HfJHsaY?si=wiR4JpS-qvY2wPcw"
+    },
+    {
+        id: 27,
+        title: "Schindler's List",
+        type: "movie",
+        img: "assets/schindler.jpg",
+        vibes: ["War","Historical","Drama","Biographical"],
+        description: "Historial drama about Oskar Schindler, who saves Jewish lives during the Holocaust by employing them in his factories.",
+        wiki: "https://en.wikipedia.org/wiki/Schindler%27s_List",
+        learnMore: "https://youtu.be/mxphAlJID9U?si=it6HHCZwBu7HGcZN"
+    },
+    {
+        id: 28,
+        title: "12 Angry Men",
+        type: "movie",
+        img: "assets/angrymen.jpg",
+        vibes: ["Courtroom Drama","Psychological Drama"],
+        description: "The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully caution forces them to more  carefully consider the evidence.",
+        wiki: "https://en.wikipedia.org/wiki/12_Angry_Men",
+        learnMore: "https://youtu.be/TEN-2uTi2c0?si=2tyCluNZwG0HBMGv"
+    },
+    {
+        id: 29,
+        title: "Death Note",
+        type: "show",
+        img: "assets/deathnote.jpg",
+        vibes: ["Psychological Thriller","Crime","Drama","Mystery","Supernatural"],
+        description: "A brilliant high school student finds a supernatural notebook that kills anyone whose name is written on it.",
+        wiki: "https://en.wikipedia.org/wiki/Death_Note_(2006_TV_series)",
+        learnMore: "https://youtu.be/NlJZ-YgAt-c?si=i-1aDEHsWA5_UCQx"
+    },
+    {
+        id:30,
+        title: "True Detective",
+        type: "show",
+        img: "assets/true_detective.png",
+        vibes: ["Crime","Drama","Mystery","Thriller","Neo-Noir"],
+        description: "An anthology crime drama exploring complex investigations, psychological trauma, and philosophical themes.",
+        wiki: "https://en.wikipedia.org/wiki/True_Detective",
+        learnMore: "https://youtu.be/fVQUcaO4AvE?si=sGNQIoVgNZdNvdNx"
+    },
+    {
+        id: 31,
+        title: "Bojack Horseman",
+        type: "show",
+        img: "assets/bojack.jpg",
+        vibes: ["Black Comedy","Dark Humour","Satire","Psychological"],
+        description: "Dark animated dramedy about a washed-up actor horse dealing with fame, depression and self-destruction.",
+        wiki: "https://en.wikipedia.org/wiki/BoJack_Horseman",
+        learnMore: "https://youtu.be/i1eJMig5Ik4?si=TXi2Ohb2Zuuw2jch"
+    },
+    {
+        id: 32,
+        title: "Nineteen Eighty-Four",
+        type: "book",
+        img: "assets/1984.jpg",
+        vibes: ["Dystopian Fiction","Oppression","Totalitarianism"],
+        description: "Orwell's dystopian novel about totalitarian surveillance, thought control, and a society ruled by 'Big Brother'.",
+        wiki: "https://en.wikipedia.org/wiki/Nineteen_Eighty-Four",
+        learnMore: "https://www.goodreads.com/book/show/61439040-1984"
+    },
+    {
+        id: 33,
+        title: "Crime and Punishment",
+        type: "book",
+        img: "assets/crimeNpunishment.png",
+        vibes: ["Guilt","Paranoia","Psychological Fiction","Literary Realism"],
+        description: "Dostoevsky's psychological novel about guilt, morality, and the mental collapse of a man after committing murder.",
+        wiki: "https://en.wikipedia.org/wiki/Crime_and_Punishment",
+        learnMore: "https://www.goodreads.com/book/show/7144.Crime_and_Punishment"
+    },
+    {
+        id: 34,
+        title: "Pride and Prejudice",
+        type: "book",
+        img: "assets/pride_and_prejudice.jpg",
+        vibes: ["Romance","Realism","Satire"],
+        description: "Austen's classic novel about love, class, and social expectations in Regency-era England.",
+        wiki: "https://en.wikipedia.org/wiki/Pride_and_Prejudice",
+        learnMore: "https://www.goodreads.com/book/show/129915654-pride-and-prejudice"
     }   
 ];
--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 console.log("DATA:",MEDIA_DATA);
+
 //HOME DATA RENDER
-const homeTrack= document.getElementById("carouselTrack");
-function renderHome(data){
-    if (!homeTrack) return;
-    homeTrack.innerHTML="";
+const keepcomingbackTrack= document.getElementById("keepcomingbackTrack");
+const trendingTrack=document.getElementById("trendingTrack");
+const critiallyacclaimedTrack=document.getElementById("criticallyacclaimedTrack");
+function renderRow(track,data){
+    if (!track) return;
+    track.innerHTML="";
     data.forEach(item => {
         const card=document.createElement("div");
         card.className="card flex-none w-36 md:w-40 lg:w-52 cursor:pointer transition duration-300 hover:scale-105";
         card.dataset.id=item.id;
         card.innerHTML=`<img src="${item.img}" alt="${item.title}" class="w-full aspect-[2/3] object-cover rounded-xl shadow-lg">`;
-        homeTrack.appendChild(card);
+        track.appendChild(card);
     });
 }
 //hiding recommendations content on home-view tab
 function showHome(){
     home-view.classList.remove("hidden");
     recommendations-view.classList.add("hidden");
-    home-view.innerHTML; renderHome();
+    home-view.innerHTML; renderRow();
 }
 function showRecommendations(){
     recommendations-view.classList.remove("hidden");
@@ -30,7 +33,7 @@ function renderExplore(data){
     exploreGrid.innerHTML="";
     data.forEach(item=>{
         const card=document.createElement("div");
-        card.className="card shrink-0 w-40 md:w-48 lg:w-56 cursor-pointer transition duration-300 hover:scale-105";
+        card.className="explore-card shrink-0 w-40 md:w-48 lg:w-56 cursor-pointer transition duration-300 hover:scale-105";
         card.dataset.id=String(item.id);
         card.innerHTML=`<img src="${item.img}" alt="${item.title}" class="w-full aspect-[2/3] rounded-xl shadow-lg object-cover">`;
         exploreGrid.appendChild(card);
@@ -38,27 +41,44 @@ function renderExplore(data){
 
 }
 // INITIAL-RENDER
+const keepcomingback=MEDIA_DATA.filter(item => [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].includes(item.id));
+const trending=MEDIA_DATA.filter(item=> [16,17,18,19,20,21,22,23,24].includes(item.id));
+const criticallyacc=MEDIA_DATA.filter(item=> [25,26,27,28,29,30,31,32,33,34].includes(item.id));
 document.addEventListener("DOMContentLoaded", ()=>{
-    renderHome(MEDIA_DATA);
+    renderRow(keepcomingbackTrack,keepcomingback);
+    renderRow(trendingTrack,trending);
+    renderRow(criticallyacclaimedTrack,criticallyacc);
     renderExplore(MEDIA_DATA);
+});
+// explore view searchbox
+
+searchinput?.addEventListener("input",()=>{
+    const query=searchinput.value.toLowerCase();
+    console.log("query:",query);
+    console.log("titles",MEDIA_DATA.map(item=>item.title));
+    const filtered=MEDIA_DATA.filter(item=>item.title.toLowerCase().includes(query));
+    console.log(filtered);
+    renderExplore(filtered);
+    if (filtered.length ===0){
+        exploreGrid.innerHTML=`<p class="text-center text-gray-400 col-span-full">No results found.</p>`;
+        return; 
+    }
 });
 // FILTER FUNCTION
 function applyFilter(filter){
-    const cards=document.querySelectorAll(".explore-card");
+    const cards=document.querySelectorAll(".card");
         cards.forEach(card=> {
             const item= MEDIA_DATA.find(m=> String(m.id) === card.dataset.id);
             if (!item) return;
             const match= filter==="all" || item.vibes.some(v => v.toLowerCase().includes(filter));
             card.classList.toggle("hidden", !match);
-        });
-    
+        });   
 }
 document.querySelectorAll(".filter-btn").forEach(btn=>{
     btn.addEventListener("click",() =>{
         applyFilter(btn.dataset.filter);
     });
 });
-
 //modal
 function openModal(item){
     if (!item) return;
@@ -70,7 +90,7 @@ function openModal(item){
     document.getElementById("modalLearnMore").href=item.learnMore;
     document.getElementById("movieModal").classList.remove("hidden");
     const vibesContainer=document.getElementById("modalVibes");
-    vibesContainer.innerHTML=item.vibes.map(vibe => `<span class="rounded-full bg-amber-500/15 border border-amber-500/30 px-3 py-1 text-sm text-amber-300">${vibe}</span>`).join("");
+    vibesContainer.innerHTML=item.vibes.map(vibe => `<span class="rounded-full bg-blue-900/15 border border-blue-700/30 px-3 py-1 text-sm text-white">${vibe}</span>`).join("");
     document.getElementById("movieModal").classList.remove("hidden");
     
 }
@@ -113,6 +133,15 @@ const navHome=document.querySelector('.n1');
 const navExplore=document.querySelector('.n2');
 const navRecommend=document.querySelector('.n4');
 const navAbout=document.querySelector('.n5');
+const heroExplore=document.getElementById("heroExplore");
+const heroRecommend=document.getElementById("heroRecommend");
+//hero section explore and recommendations buttons 
+heroExplore?.addEventListener("click",()=>{
+    showView(viewExplore);
+});
+heroRecommend?.addEventListener("click",()=>{
+    showView(viewRecommend);
+});
 
 const viewHome=document.getElementById('home-view');
 const viewExplore=document.getElementById('explore-view');
@@ -120,6 +149,7 @@ const viewRecommend=document.getElementById('recommendations-view');
 const viewAbout=document.getElementById('about-view');
 const allViews=[viewHome,viewExplore,viewRecommend,viewAbout];
 function showView(view){
+    console.log("showing:",view);
     allViews.forEach(v=>v?.classList.add("hidden"));
     view?.classList.remove("hidden");
 }
@@ -139,13 +169,76 @@ navAbout?.addEventListener('click',(e)=>{
     e.preventDefault();
     showView(viewAbout);
 });
-//dark and light mode
-const themeToggle=document.getElementById('themeToggle');
-themeToggle?.addEventListener('click',()=>{
-    document.documentElement.classList.toggle('dark');
-    const isDark=document.documentElement.classList.contains("dark");
-    themeToggle.textContent=isDark
-         ? "\u2600 Light Mode"
-         : "\u263E Dark Mode"
-});
 
+// recommendation-view stuff
+const cozyRowTrack=document.getElementById("cozyRowTrack");
+const teenRowTrack=document.getElementById("teenRowTrack");
+const inputel=document.getElementById("moodInput");
+const buttonel=document.getElementById("recommendBtn");
+const resultsel=document.getElementById("recommendResults");
+function getRecommendations(input){
+    const query=input.toLowerCase();
+    return MEDIA_DATA
+       .map(item=>{
+        let score=0;
+        item.vibes.forEach(vibe =>{
+            if(query.includes(vibe.toLowerCase())){
+                score +=1;
+            }
+        });
+        return {...item,score};
+           })
+           .filter(item => item.score >0)
+           .sort((a,b)=> b.score - a.score);
+}
+function renderResults(items){
+    resultsel.innerHTML="";
+    if (items.length ===0){
+        resultsel.innerHTML="<p>No matches found. Try different words.</p>";
+        return;
+    }
+    items.forEach(item=>{
+        const card=document.createElement("div");
+        card.className="card bg-zinc-900 rounded-xl shadow-lg overflow-hidden max-w-sm w-full";
+        card.innerHTML=`<img src="${item.img}" class="w-full h-56 object-cover" alt="${item.title}" />
+        <h3>${item.title}</h3>
+        <br>
+        <p>${item.description}</p>
+        <br>
+        <div>
+           ${item.vibes.map(v=>`<span class="tag rounded-full bg-zinc-900/15 border border-blue-700/30 px-3 py-1 text-sm text-blue-900">${v}</span>`).join(" ")}
+        </div>
+        <br>
+        <a href="${item.learnMore}" target="_blank">LearnMore</a>
+        `;
+        resultsel.appendChild(card);
+    });
+}
+function renderRow2(track,data){
+    if (!track) return;
+    track.innerHTML="";
+    data.forEach(item => {
+        const card=document.createElement("div");
+        card.className="card flex-none w-36 md:w-40 lg:w-52 cursor:pointer transition duration-300 hover:scale-105";
+        card.dataset.id=item.id;
+        card.innerHTML=`<img src="${item.img}" alt="${item.title}" class="w-full aspect-[2/3] object-cover rounded-xl shadow-lg">`;
+        track.appendChild(card);
+    });
+}
+const cozyRow=MEDIA_DATA.filter(item=> [2,6,5,20,13].includes(item.id));
+const teenRow=MEDIA_DATA.filter(item=> [3,4,18,19,2].includes(item.id));
+document.addEventListener("DOMContentLoaded", ()=>{
+    renderRow2(cozyRowTrack,cozyRow);
+    renderRow2(teenRowTrack,teenRow);   
+});
+buttonel.addEventListener("click",()=>{
+    const input=inputel.value.trim();
+    if(!input) return;
+    const results=getRecommendations(input);
+    renderResults(results);
+});
+const recommendBtn=document.getElementById('recommendBtn');
+const storiesContainer=document.getElementById('stories');
+recommendBtn.addEventListener('click',()=>{
+    storiesContainer.classList.add('hidden');
+});
